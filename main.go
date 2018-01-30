5f78255e5045b761742bf71c80372b9738b79af7
yesq <qiaopeng01@gmail.com>
2018-01-30 21:49:19 +0800

    mine

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"bytes"
 	"crypto/sha256"
 	"fmt"
+	"math"
+	"math/big"
 	"strconv"
 	"time"
 )
@@ -13,23 +15,37 @@ type Block struct {
 	Data          []byte
 	PrevBlockHash []byte
 	Hash          []byte
+	Nonce         int
 }
 
 type Blockchain struct {
 	blocks []*Block
 }
 
-func (b *Block) SetHash() {
-	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
-	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
-	hash := sha256.Sum256(headers)
-
-	b.Hash = hash[:]
+type ProofOfWork struct {
+	block  *Block
+	target *big.Int
 }
 
+const targetBits = 10
+
+// func (b *Block) SetHash() {
+// 	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
+// 	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
+// 	hash := sha256.Sum256(headers)
+
+// 	b.Hash = hash[:]
+// }
+
 func NewBlock(data string, PrevBlockHash []byte) *Block {
-	block := &Block{time.Now().Unix(), []byte(data), PrevBlockHash, []byte{}}
-	block.SetHash()
+	block := &Block{time.Now().Unix(), []byte(data), PrevBlockHash, []byte{}, 0}
+	// block.SetHash()
+	pow := NewProofOfWork(block)
+	nonce, hash := pow.Run()
+
+	block.Hash = hash[:]
+	block.Nonce = nonce
+
 	return block
 }
 
@@ -45,6 +61,59 @@ func NewGenesisBlock() *Block {
 
 func NewBlockchain() *Blockchain {
 	return &Blockchain{[]*Block{NewGenesisBlock()}}
+}
+
+func NewProofOfWork(b *Block) *ProofOfWork {
+	target := big.NewInt(1)
+	target.Lsh(target, uint(256-targetBits))
+
+	pow := &ProofOfWork{b, target}
+
+	return pow
+}
+
+func (pow *ProofOfWork) prepareData(nonce int) []byte {
+	data := bytes.Join(
+		[][]byte{
+			pow.block.PrevBlockHash,
+			pow.block.Data,
+			IntToHex(pow.block.Timestamp),
+			IntToHex(int64(targetBits)),
+			IntToHex(int64(nonce)),
+		},
+		[]byte{},
+	)
+
+	return data
+}
+
+func IntToHex(n int64) []byte {
+	return []byte(strconv.FormatInt(n, 10))
+}
+
+func (pow *ProofOfWork) Run() (int, []byte) {
+	var hashInt big.Int
+	var hash [32]byte
+	nonce := 0
+
+	fmt.Printf("Mining the block containing \"%s\"\n", pow.block.Data)
+	for nonce < math.MaxInt64 {
+		data := pow.prepareData(nonce)
+		hash = sha256.Sum256(data)
+		fmt.Println()
+		fmt.Printf("\r\r%x   %d\n  %x", hash, nonce, pow.target)
+		hashInt.SetBytes(hash[:])
+
+		if hashInt.Cmp(pow.target) == -1 {
+			fmt.Printf("\ngot it\n")
+			break
+		} else {
+			nonce++
+		}
+	}
+	fmt.Printf("\n\n")
+
+	return nonce, hash[:]
 }
 
 func main() {
